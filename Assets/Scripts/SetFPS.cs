7fcebdcd6857aa545d527a00410df94e70c573b1
yayoimizuha <poko.peko.tomo@icloud.com>
2022-11-22 19:26:55 +0900

    add pull down & explanation.

---
+++ Assets/Scripts/SetFPS.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFPS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
